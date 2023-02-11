cf0abfc3daae2637fbc6329a136846defb7fd3df
Надим Шакиржанов~ <aversul@gmail.com>
2023-02-11 20:06:33 +0400

    commit_1

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -30,13 +30,13 @@ class UserViewSet(viewsets.ModelViewSet):
     )
     def me(self, request):
         user = get_object_or_404(User, username=self.request.user)
-        if request.method == 'GET':
-            serializer = MeSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == 'PATCH':
             serializer = MeSerializer(user, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.method == 'GET':
+            serializer = MeSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
